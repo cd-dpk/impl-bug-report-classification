ee3c42c09415ef2563e7d20c4f0ea5f42e5dd222
DIPOK <chandradasdipok@gmail.com>
2018-01-28 03:00:26 +0600

    voting scores well

--- src/fse17/sum_des_stacking/exp.py
+++ src/fse17/sum_des_stacking/exp.py
@@ -16,12 +16,7 @@ target_arr = np.array([], dtype=str)
 def setFeatureNamesAndRows(file_name):
     with open(file_name+'_vec.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        ## set features names
-        # print(reader.fieldnames)
         feature_names_arr = np.array(reader.fieldnames)
-
-        # print(feature_names_arr)
-
         target_column = intent
         if feature_names_arr[len(feature_names_arr) - 1] == target_column:
             feature_names_arr = np.delete(feature_names_arr, len(feature_names_arr) - 1, axis=0)
@@ -31,11 +26,6 @@ def setFeatureNamesAndRows(file_name):
             row_count += 1
 
         chou_data[feature_names] = feature_names_arr
-        # print(chou_data[feature_names])
-
-        ##initialize empty np_array(data,target) with shape of row_count and feature_count
-
-        # print(str(row_count)+','+str(len(feature_names_arr)))
         data_arr = np.empty([row_count, len(feature_names_arr)], dtype=str)
         target_arr = np.empty([row_count], dtype=str)
         chou_data[data] = data_arr
@@ -156,6 +146,9 @@ def confusion_matrix(y_test,y_predict):
 
     return {'t_p':t_p,'f_p':f_p,'t_n':t_n,'f_n':f_n}
 
+def calc_tuple(result_dic:dict):
+    return (result_dic['t_p'],result_dic['t_n'],result_dic['f_p'],result_dic['f_n'])
+
 def calc_pre_rec(result_dic:dict):
     t_p = result_dic['t_p']
     t_n = result_dic['t_n']
@@ -169,35 +162,43 @@ def calc_pre_rec(result_dic:dict):
     else:
         return (0.0,0.0)
 
-
+def calc_test(result_dic:dict):
+    t_p = result_dic['t_p']
+    t_n = result_dic['t_n']
+    f_p = result_dic['f_p']
+    f_n = result_dic['f_n']
+    return (t_p,t_n)
 
 def doExperiment(file):
     print(load_data(file))
     print(Counter(chou_data[target]))
-    #exit()
-
 
     from sklearn.linear_model import LogisticRegression
-
     from sklearn.ensemble import RandomForestClassifier
     from sklearn.naive_bayes import GaussianNB
     from sklearn.neighbors import KNeighborsClassifier
     from sklearn import svm
     from sklearn.ensemble import GradientBoostingClassifier
     from sklearn.ensemble import AdaBoostClassifier
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.feature_selection import SelectKBest
+    from sklearn.feature_selection import chi2
 
     X = chou_data[data]
     y = chou_data[target]
+    #X = SelectKBest(chi2, k=200).fit_transform(chou_data[data], y)
 
     X_folds = np.array_split(X, 10)
     y_folds = np.array_split(y, 10)
 
-    pre = 0.0
-    rec = 0.0
+    t_p = 0.0
+    f_p = 0.0
+    t_n = 0.0
+    f_n = 0.0
 
-    H = LogisticRegression()
-    h1 = RandomForestClassifier(max_depth=2);
-    h2 = GaussianNB()
+    h = LogisticRegression()
+    h1 = GaussianNB()
+    h2 = RandomForestClassifier(max_depth=2)
     h3 = KNeighborsClassifier(5, weights='distance')
     h4 = svm.SVC()
     h5 = GradientBoostingClassifier()
@@ -213,99 +214,53 @@ def doExperiment(file):
         y_test = y_train.pop(k)
         y_train = np.concatenate(y_train)
 
-        X_folds_2 = np.array_split(X_train, 10)
-        y_folds_2 = np.array_split(y_train, 10)
+        X_s, y_s = under_sampling(X_train, y_train)
+        if(sys.argv[2]=='o'):
+            X_s, y_s = over_sampling(X_train, y_train)
+        elif((sys.argv[2]=='s')):
+            X_s, y_s = smote(X_train, y_train)
 
-        stacking_target = y_train
-        stacking_data = np.empty([len(stacking_target),6],dtype=int)
-        training_counter = 0
-
-        for l in range(10):
-            X_train_2 = list(X_folds_2)
-            X_test_2 = X_train_2.pop(l)
-            X_train_2 = np.concatenate(X_train_2)
-            y_train_2 = list(y_folds_2)
-            y_test_2 = y_train_2.pop(l)
-            y_train_2 = np.concatenate(y_train_2)
-
-            ## under_sampling ##
-            X_s, y_s = under_sampling(X_train_2, y_train_2)
-
-            h1.fit(X_s, y_s)
-            y1_predict = h1.predict(X_test_2)
+        h.fit(X_s,y_s)
+        if(sys.argv[3] == '1'):
+            h1.fit(X_s,y_s)
+            h = h1
+        elif (sys.argv[3] == '2'):
             h2.fit(X_s, y_s)
-            y2_predict = h2.predict(X_test_2)
+            h = h2
+        elif (sys.argv[3] == '3'):
             h3.fit(X_s, y_s)
-            y3_predict = h3.predict(X_test_2)
-            h4.fit(X_s,y_s)
-            y4_predict = h4.predict(X_test_2)
-            h5.fit(X_s,y_s)
-            y5_predict = h5.predict(X_test_2)
+            h = h3
+        elif (sys.argv[3] == '4'):
+            h4.fit(X_s, y_s)
+            h = h4
+        elif (sys.argv[3] == '5'):
+            h5.fit(X_s, y_s)
+            h = h5
+        elif (sys.argv[3] == '6'):
             h6.fit(X_s,y_s)
-            y6_predict = h6.predict(X_test_2)
+            h = h6
 
-            for row in range(len(y_test_2)):
-                stacking_data[training_counter+row][0] = y1_predict[row]
-                stacking_data[training_counter+row][1] = y2_predict[row]
-                stacking_data[training_counter+row][2] = y3_predict[row]
-                stacking_data[training_counter+row][3] = y4_predict[row]
-                stacking_data[training_counter+row][4] = y5_predict[row]
-                stacking_data[training_counter+row][5] = y6_predict[row]
-                stacking_target[training_counter] = y_test_2[row]
+        y_predict = h.predict(X_test)
+        print(confusion_matrix(y_test, y_predict))
+        temp_tp, temp_tn,temp_fp,temp_fn = calc_tuple(confusion_matrix(y_test, y_predict))
+        t_p += temp_tp
+        t_n += temp_tn
+        f_p += temp_fp
+        f_n += temp_fn
 
-            training_counter += len(y_test_2)
+    print(Counter(y))
 
+    print(t_p,t_n,f_p,f_n)
 
-        ### stacking ###
-        H.fit(stacking_data,stacking_target)
-
-
-        ## predicting
-
-        y1_predict = h1.predict(X_test)
-        y2_predict = h2.predict(X_test)
-        y3_predict = h3.predict(X_test)
-        y4_predict = h4.predict(X_test)
-        y5_predict = h5.predict(X_test)
-        y6_predict = h6.predict(X_test)
-
-        stacking_target_test = y_test
-        stacking_data_test = np.empty([len(X_test),6],dtype=int)
-
-        for x in range(len(y_test)):
-            stacking_data_test[x][0] = y1_predict[x]
-            stacking_data_test[x][1] = y2_predict[x]
-            stacking_data_test[x][2] = y3_predict[x]
-            stacking_data_test[x][3] = y4_predict[x]
-            stacking_data_test[x][4] = y5_predict[x]
-            stacking_data_test[x][5] = y6_predict[x]
-
-        predicted_probability = H.predict_proba(stacking_data_test)
-        print(predicted_probability)
-
-        threshold = 0.3
-        #for y in range(10):
-        print(threshold)
-        y_predict = np.empty(len(predicted_probability),dtype=int)
-        for x in range(len(predicted_probability)):
-            if predicted_probability[x][1] >= threshold and predicted_probability[x][1] >= predicted_probability[x][0]:
-                y_predict[x] = 1
-            else:
-                y_predict[x] = 0
-
-        print(confusion_matrix(stacking_target_test, y_predict))
-        temp_pre, temp_rec = calc_pre_rec(confusion_matrix(stacking_target_test, y_predict))
-        pre += temp_pre
-        rec += temp_rec
-
-        #threshold += 0.05
-
-    print(pre/10.0)
-    print(rec/10.0)
-
-
+    print((t_p+t_n)/(t_p+t_n+f_p+f_n))
+    print((t_p)/(t_p+f_p))
+    print((t_p) / (t_p + f_n))
 ''' Experiment Ends here '''
 
-subject= sys.argv[1]
-intent= sys.argv[2]
-doExperiment(subject)
+if(len(sys.argv)==5):
+    subject= sys.argv[1]
+    intent= sys.argv[4]
+    doExperiment(subject)
+else:
+    print('parameter mistmatch')
+    exit()
--- src/init.py
+++ src/init.py
@@ -11,3 +11,12 @@ if 3 not in x and 2 not in x:
     print('No')
 else:
     print('Yes')
+
+
+from collections import Counter
+
+y = [0,1,0,0]
+c = Counter(y)
+keys = c.keys()
+for i in keys:
+    print(c.__getitem__(i))
