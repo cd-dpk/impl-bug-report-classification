cb0125d3a668f4ce9c8055a8acac75e9935eff66
Dipok Chandra Das <bsse0501@iit.du.ac.bd>
2018-02-06 23:23:06 +0000

    exp_NB.py edited online with Bitbucket

--- src/aggregate/exp_NB.py
+++ src/aggregate/exp_NB.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys,csv
 from collections import Counter
+from src.aggregate.chou_data import ChouDataHandler
 
 ambari = 'ambari'
 camel = 'camel'
@@ -11,64 +12,9 @@ Surprising = 'Surprising'
 Security = 'Security'
 Performance = 'Performance'
 
+
 subject = ''
 intent = ''
-chou_data = {}
-feature_names = 'feature_names'
-target_names = 'target_names'
-target = 'target'
-data = 'data'
-
-data_arr = np.array([], dtype=object)
-target_arr = np.array([], dtype=object)
-
-def setFeatureNamesAndRows(file_name):
-    with open(file_name+'_vec.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        feature_names_arr = np.array(reader.fieldnames)
-        target_column = intent
-        if feature_names_arr[len(feature_names_arr) - 1] == target_column:
-            feature_names_arr = np.delete(feature_names_arr, len(feature_names_arr) - 1, axis=0)
-
-        row_count = 0
-        for row in reader:
-            row_count += 1
-
-        chou_data[feature_names] = feature_names_arr
-        data_arr = np.empty([row_count, len(feature_names_arr)], dtype=object)
-        target_arr = np.empty([row_count], dtype=object)
-        chou_data[data] = data_arr
-        chou_data[target] = target_arr
-        return
-
-
-def load_data(file):
-    setFeatureNamesAndRows(file)
-    with open(file+'_vec.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        # set target names
-        target_names_arr = np.array([intent, 'Not'+intent])
-        chou_data[target_names] = target_names_arr
-
-        rw_counter = 0
-        # set data and target
-        for row in reader:
-            f_counter = 0
-            data_arr_row = np.empty([len(chou_data[feature_names])], dtype=object)
-            features = chou_data[feature_names]
-            for x in features:
-                if x in ('reporter','component'):
-                    data_arr_row[f_counter] = row[x]
-                elif row[x] not in (None, ''):
-                    data_arr_row[f_counter] = int(row[x],10)
-
-                f_counter += 1
-
-            chou_data[data][rw_counter] = data_arr_row
-            chou_data[target][rw_counter] = int(row[intent],10)
-            rw_counter += 1
-
-    return chou_data
 
 def under_sampling(X, y):
         from imblearn.under_sampling import RandomUnderSampler
@@ -136,20 +82,29 @@ def calc_test(result_dic:dict):
     return (t_p,t_n)
 
 def doExperiment(file):
-    print(load_data(file))
-    print(Counter(chou_data[target]))
+    chou_data = ChouDataHandler()
+    chou_data.load_data(file)
+
+    print(chou_data.textual_data)
+    print(Counter(chou_data.target_data))
 
     from sklearn.naive_bayes import MultinomialNB
-    from sklearn.feature_selection import chi2
-    from sklearn.feature_selection import SelectKBest
-    X = chou_data[data]
-    y = chou_data[target]
+    from sklearn.naive_bayes import GaussianNB
+    X_text_features = chou_data.textual_data
+    y_target = chou_data.target_data
+    X_reporter = []
+    for x in chou_data.reporter_to_numeric_data():
+        X_reporter.append(np.array([x],dtype=int))
+    X_reporter = np.array(X_reporter)
+    print(X_reporter)
+    X_component = chou_data.component_to_numeric_data()
 
     #X = SelectKBest(chi2(X,y),k=200)
 
-    X_folds = np.array_split(X, 10)
-    y_folds = np.array_split(y, 10)
-
+    X_text_features_folds = np.array_split(X_text_features, 10)
+    y_target_folds = np.array_split(y_target, 10)
+    X_reporter_folds = np.array_split(X_reporter, 10)
+    X_component_folds = np.array_split(X_component, 10)
     t_p = 0.0
     f_p = 0.0
     t_n = 0.0
@@ -158,21 +113,49 @@ def doExperiment(file):
     h = MultinomialNB()
     for k in range(10):
         # We use 'list' to copy, in order to 'pop' later on
-        X_train = list(X_folds)
-        X_test = X_train.pop(k)
-        X_train = np.concatenate(X_train)
-        y_train = list(y_folds)
+        X_train_text = list(X_text_features_folds)
+        X_test_text = X_train_text.pop(k)
+        X_train_text = np.concatenate(X_train_text)
+
+        X_train_reporter = list(X_reporter_folds)
+        X_test_reporter = X_train_reporter.pop(k)
+        X_train_reporter = np.concatenate(X_train_reporter)
+
+        print(X_train_reporter)
+
+        X_train_component = list(X_component_folds)
+        X_test_component = X_train_component.pop(k)
+        X_train_component = np.concatenate(X_train_component)
+
+        y_train = list(y_target_folds)
         y_test = y_train.pop(k)
         y_train = np.concatenate(y_train)
 
-        X_s, y_s = under_sampling(X_train, y_train)
+        X_s, y_s = under_sampling(X_train_text, y_train)
         #X_s, y_s = over_sampling(X_train, y_train)
         #X_s, y_s = smote(X_train, y_train)
-
         h.fit(X_s,y_s)
-        y_predict = h.predict(X_test)
+        y1_predict = h.predict_proba(X_test_text)
+
+        X_s, y_s = under_sampling(X_train_component, y_train)
+        h.fit(X_s,y_s)
+        y2_predict = h.predict_proba(X_test_component)
+
+        X_s, y_s = under_sampling(X_train_reporter, y_train)
+        h.fit(X_s, y_s)
+        y3_predict = h.predict_proba(X_test_reporter)
+
+        y_predict = np.empty(len(y1_predict),int)
+
+        for x in range(len(y_predict)):
+            if y1_predict[x][1]+y2_predict[x][1]+y3_predict[x][1] >= y1_predict[x][0]+y2_predict[x][0]+y3_predict[x][0]:
+                y_predict[x] = 1
+            else:
+                y_predict[x] = 0
+
+        print(y_predict)
         print(confusion_matrix(y_test, y_predict))
-        temp_tp, temp_tn,temp_fp,temp_fn = calc_tuple(confusion_matrix(y_test, y_predict))
+        temp_tp, temp_tn, temp_fp, temp_fn = calc_tuple(confusion_matrix(y_test, y_predict))
         t_p += temp_tp
         t_n += temp_tn
         f_p += temp_fp
